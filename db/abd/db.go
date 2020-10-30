acbdc7585d87715f934968c8d0ae16f53c9c714f
Saidi Tang <tangsaidi@icloud.com>
2020-10-30 11:18:54 -0700

    fixed error message

--- db/abd/db.go
+++ db/abd/db.go
@@ -122,7 +122,7 @@ func (r *abd) BatchRead(ctx context.Context, table string, keys []string, fields
 	gobReader := gob.NewDecoder(r.reader)
 	var response []Value
 	if err := gobReader.Decode(&response); err != nil {
-		fmt.Println("Error when inserting:", err)
+		fmt.Println("Error when reading:", err)
 	}
 	return nil, nil
 }
